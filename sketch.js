9eeaa0c9cba549d40dd40dd8c045861361508f23
180608ar <73707937+180608ar@users.noreply.github.com>
2021-02-12 21:21:28 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+}
+
